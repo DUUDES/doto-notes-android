c83c89081a3dc0ab604e20c30eb73e8f402dc611
Mikhail Bovkun <mikhailbovkun@gmail.com>
2023-05-20 01:28:15 +0300

    Додав навігацію по сторінкам

--- app/src/main/java/uni/digi2/dotonotes/MainActivity.kt
+++ app/src/main/java/uni/digi2/dotonotes/MainActivity.kt
@@ -5,16 +5,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
@@ -34,15 +54,21 @@ class MainActivity : ComponentActivity() {
         val firebaseAuth = FirebaseAuth.getInstance()
 
         setContent {
-            FirebaseUIAuthScreen(
-                firebaseAuth = firebaseAuth,
-                signInProviders = providers,
-                onSignInSuccess = { /* Дії після успішної автентифікації */
-                                  setContent {
-                                      Greeting("Android")
-                                  }},
-                onSignInFailure = { error -> /* Дії після неуспішної автентифікації */ }
-            )
+            val navController = rememberNavController()
+            if (FirebaseAuth.getInstance().currentUser == null) {
+                FirebaseUIAuthScreen(
+                    firebaseAuth = firebaseAuth,
+                    signInProviders = providers,
+                    onSignInSuccess = {
+                        setContent {
+                            BottomNavigationApp(navController)
+                        }
+                    },
+                    onSignInFailure = { error -> /* Дії після неуспішної автентифікації */ }
+                )
+            } else {
+                BottomNavigationApp(navController)
+            }
         }
     }
 }
@@ -103,35 +129,79 @@ fun FirebaseUIAuthScreen(
 }
 
 
+@Composable
+fun HomeScreen() {
+    Greeting("Home")
+    // Вміст головної сторінки
+}
+@Composable
+fun ProfileScreen() {
+    Greeting("Profile")
+    // Вміст профілю користувача
+}
 
-//@Composable
-//private fun ShowFragment() {
-//
-//    if(FirebaseAuth.getInstance().currentUser == null){
-//        // відкриваємо вікно логіну
-//        DoLogin()
-//
-//
-//
-//    }else{
-//        // відкриваємо вікно з нотатками
-//        Greeting("User")
-//    }
-//}
-
-
-//val providers = arrayListOf(
-//    AuthUI.IdpConfig.GoogleBuilder().build()
-//)
-
-//@Composable
-//private fun DoLogin(){
-//    val signInIntent = AuthUI.getInstance()
-//        .createSignInIntentBuilder()
-//        .setAvailableProviders(providers)
-//        .setIsSmartLockEnabled(false)
-//        .build()
-//}
+@Composable
+fun AppNavHost(navController: NavController) {
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen()
+        }
+        composable("profile") {
+            ProfileScreen()
+        }
+    }
+}
 
 
 
+val LocalNavController = compositionLocalOf<NavController> { error("No NavController found") }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavigationApp(navController: NavController) {
+    val items = listOf(
+        Screen.Home,
+        Screen.Profile
+    )
+
+    CompositionLocalProvider(LocalNavController provides navController) {
+        Scaffold(
+            bottomBar = {
+                BottomNavigation {
+                    val currentRoute = LocalNavController.current.currentDestination?.route
+
+                    items.forEach { screen ->
+                        val selected = currentRoute == screen.route
+                        val onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                            }
+                        }
+
+                        BottomNavigationItem(
+                            icon = { Icon(screen.icon, contentDescription = screen.title) },
+                            label = { Text(screen.title) },
+                            selected = selected,
+                            onClick = onClick
+                        )
+                    }
+                }
+            }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                AppNavHost(navController = navController)
+            }
+        }
+    }
+}
+
+sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
+    object Home : Screen("home", "Головна сторінка", Icons.Default.Home)
+    object Profile : Screen("profile", "Профіль", Icons.Default.Person)
+}
