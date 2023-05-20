01dac3a8d79bb8537bc2aceac2a9149d98e0196d
Mikhail Bovkun <mikhailbovkun@gmail.com>
2023-05-21 00:22:21 +0300

    Додав сплеш скрін

--- app/src/main/java/uni/digi2/dotonotes/MainActivity.kt
+++ app/src/main/java/uni/digi2/dotonotes/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -17,6 +18,7 @@ import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import uni.digi2.dotonotes.ui.BottomNavigationApp
 import uni.digi2.dotonotes.ui.screens.authorization.FirebaseUIAuthScreen
+import uni.digi2.dotonotes.ui.screens.splash.SplashViewModel
 import uni.digi2.dotonotes.ui.screens.tasks.TodoListScreen
 import uni.digi2.dotonotes.ui.screens.tasks.TodoViewModel
 
@@ -26,6 +28,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModel = SplashViewModel()
+
+        installSplashScreen().setKeepOnScreenCondition { viewModel.isLoading.value }
 
         val providers = listOf(
             AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
