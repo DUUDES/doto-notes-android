33c6278cdefceeb5760ca5c954de6b47d5e1a5a7
danilaSADev <danilas25up@gmail.com>
2023-05-24 00:39:23 +0300

    #25 : implemented priorities for tasks

--- app/src/main/java/uni/digi2/dotonotes/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/uni/digi2/dotonotes/ui/screens/profile/ProfileScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
---
+++ app/src/main/java/uni/digi2/dotonotes/ui/screens/tasks/TodoDialogs.kt
@@ -0,0 +1,144 @@
+package uni.digi2.dotonotes.ui.screens.tasks
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import uni.digi2.dotonotes.data.tasks.TodoTask
+
+
+@Composable
+fun CreateTaskDialog(
+    onTaskCreated: (TodoTask) -> Unit,
+    onDismiss: () -> Unit
+) = TaskDialog(
+    task = null,
+    label = "Create ToDo",
+    onSubmit = onTaskCreated,
+    onDismiss = onDismiss
+)
+
+@Composable
+fun UpdateTaskDialog(
+    todoTask: TodoTask,
+    onTaskUpdated: (TodoTask) -> Unit,
+    onDismiss: () -> Unit
+) = TaskDialog(
+    task = todoTask,
+    label = "Edit ToDo",
+    onSubmit = onTaskUpdated,
+    onDismiss = onDismiss
+)
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskDialog(
+    task: TodoTask?,
+    label: String,
+    onSubmit: (TodoTask) -> Unit,
+    onDismiss: () -> Unit
+) {
+    var taskTitle by remember { mutableStateOf(task?.title ?: "") }
+    var taskDescription by remember { mutableStateOf(task?.description ?: "") }
+    var taskPriority by remember { mutableStateOf(task?.priority ?: TaskPriority.NONE.priority) }
+    var dropdownExpanded by remember { mutableStateOf(false) }
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(label) },
+        text = {
+            Column {
+                TextField(
+                    value = taskTitle,
+                    onValueChange = { newValue -> taskTitle = newValue },
+                    label = { Text("Task Title") }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                TextField(
+                    value = taskDescription,
+                    onValueChange = { newValue -> taskDescription = newValue },
+                    label = { Text("Description") }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                ExposedDropdownMenuBox(
+                    expanded = dropdownExpanded,
+                    onExpandedChange = { dropdownExpanded = !dropdownExpanded }) {
+                    TextField(
+                        value = TaskPriority.getByValue(taskPriority)?.name ?: "None",
+                        onValueChange = {},
+                        readOnly = true,
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = dropdownExpanded) },
+                        modifier = Modifier.menuAnchor()
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = dropdownExpanded,
+                        onDismissRequest = { dropdownExpanded = false })
+                    {
+                        TaskPriority.values().forEach { priority ->
+                            DropdownMenuItem(
+                                text = { Text(priority.name) },
+                                onClick = {
+                                    taskPriority = priority.ordinal
+                                    dropdownExpanded = false
+                                }
+                            )
+                        }
+                    }
+                }
+
+            }
+        },
+        confirmButton = {
+            Button(
+                onClick = {
+                    if (taskTitle.isNotBlank()) {
+                        onSubmit(
+                            task?.copy(
+                                title = taskTitle,
+                                description = taskDescription,
+                                priority = taskPriority
+                            ) ?: TodoTask(
+                                title = taskTitle,
+                                description = taskDescription,
+                                priority = taskPriority
+                            )
+                        )
+                        onDismiss()
+                    }
+                }
+            ) {
+                Text("Submit!")
+            }
+        },
+        dismissButton = {
+            Button(
+                onClick = { onDismiss() }
+            ) {
+                Text("Cancel")
+            }
+        }
+    )
+}
